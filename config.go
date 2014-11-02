403e4c01312737d343611af7ed280c00ee0e3bb3
Remon van de Kamp <rpkamp@gmail.com>
2014-11-02 20:54:44 +0100

    Finished up configuration code and tests

--- config.go
+++ config.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"code.google.com/p/gcfg"
+	"errors"
 	"fmt"
 	"github.com/rdwilliamson/aws"
 )
 
+/**
+ * Main Config struct for program
+ */
 type Config struct {
 	Threads struct {
 		Hash   int
@@ -16,21 +20,29 @@ type Config struct {
 		Access string
 	}
 	Backup map[string]*struct {
-		Enabled   *bool
-		Region    MyAwsRegion
-		Path      string
-		Db        string
-		Exclude   string
-		Include   string
-		AwsAccess *string
-		AwsSecret *string
+		Region     MyAwsRegion
+		Path       string
+		Db         string
+		Exclude    []string
+		Include    []string
+		Aws_Access string
+		Aws_Secret string
 	}
 }
 
+/**
+ * Simple wrapper for aws.Region
+ * Allowing us to add a custom unmarshal method
+ */
 type MyAwsRegion struct {
 	*aws.Region
 }
 
+/**
+ * Custom unmarshaller for MyAwsRegion
+ * Look up region in aws.Regions map
+ * @return error Returns error if region does not exist in aws.Regions
+ */
 func (m *MyAwsRegion) UnmarshalText(text []byte) error {
 	region := string(text)
 	for _, r := range aws.Regions {
@@ -42,9 +54,17 @@ func (m *MyAwsRegion) UnmarshalText(text []byte) error {
 	return fmt.Errorf("Unable to find region %s", region)
 }
 
+/**
+ * Read config from a string
+ * @param string The configuration definition
+ * @return Config A Config struct, or nil if something went wrong
+ * @return error Returns error if something went wrong. Config will be nil in this case.
+ */
 func ReadConfig(configDef string) (*Config, error) {
 	cfg := Config{}
-	err := gcfg.ReadStringInto(&cfg, configDef)
+	if err := gcfg.ReadStringInto(&cfg, configDef); err != nil {
+		return nil, err
+	}
 
 	if cfg.Threads.Hash < 1 {
 		return nil, fmt.Errorf("Need at least one hash thread")
@@ -53,5 +73,40 @@ func ReadConfig(configDef string) (*Config, error) {
 		return nil, fmt.Errorf("Need at least one upload thread")
 	}
 
-	return &cfg, err
+	if len(cfg.Backup) == 0 {
+		return nil, errors.New("No configurations given")
+	}
+
+	for key, backup := range cfg.Backup {
+		if backup.Region.Region == nil {
+			return nil, fmt.Errorf("No region supplied for config `%s`", key)
+		}
+
+		if backup.Path == "" {
+			return nil, fmt.Errorf("No path supplied for config `%s`", key)
+		}
+
+		if backup.Db == "" {
+			return nil, fmt.Errorf("No db supplied for config `%s`", key)
+		}
+
+		if backup.Aws_Access != "" && backup.Aws_Secret == "" {
+			return nil, fmt.Errorf("AWS Access code suplied, but no AWS Secret for config `%s`", key)
+		}
+
+		if backup.Aws_Access == "" && backup.Aws_Secret != "" {
+			return nil, fmt.Errorf("AWS Secret suplied, but no AWS Access code for config `%s`", key)
+		}
+
+		if backup.Aws_Access == "" && backup.Aws_Secret == "" {
+			if cfg.Aws.Access != "" && cfg.Aws.Secret != "" {
+				backup.Aws_Access = cfg.Aws.Access
+				backup.Aws_Secret = cfg.Aws.Secret
+			} else {
+				return nil, fmt.Errorf("No AWS credentials supplied for backup `%s`", key)
+			}
+		}
+	}
+
+	return &cfg, nil
 }
--- config_test.go
+++ config_test.go
@@ -4,21 +4,22 @@ import (
 	"testing"
 )
 
-func TestSimpleConfig(t *testing.T) {
+func TestBaseConfig(t *testing.T) {
 	configDef := `
     [threads]
     hash = 10
     upload = 2
 
     [aws]
-    access = abc123
-    secret = abc123
+    access = abc123Access
+    secret = abc123Secret
 
     [backup "test"]
     region = eu-west-1
     path = /tmp/
     db = tmp.db
-    exclude = *.png,*.tiff
+    exclude = *.png
+    exclude = *.tiff
     include = *.jpg
 `
 
@@ -32,6 +33,26 @@ func TestSimpleConfig(t *testing.T) {
 		return
 	}
 
+	if config.Threads.Hash != 10 {
+		t.Errorf("Invalid number of hash threads `%d`, expected `%d`", config.Threads.Hash, 10)
+	}
+
+	if config.Threads.Upload != 2 {
+		t.Errorf("Invalid number of upload threads `%d`, expected `%d`", config.Threads.Hash, 2)
+	}
+
+	if config.Aws.Access != "abc123Access" {
+		t.Errorf("Invalid AWS access code `%s`, expected `%s`", config.Aws.Access, "abc123Access")
+	}
+
+	if config.Aws.Secret != "abc123Secret" {
+		t.Errorf("Invalid AWS secret code `%s`, expected `%s`", config.Aws.Secret, "abc123Secret")
+	}
+
+	if config.Backup["test"].Region.Region.Name != "eu-west-1" {
+		t.Errorf("Invalid region `%d`, expected `%d`", config.Backup["test"].Region.Region.Name, "eu-west-1")
+	}
+
 	if config.Backup["test"].Path != "/tmp/" {
 		t.Errorf("Invalid path `%s`, expected `%s`", config.Backup["test"].Path, "/tmp/")
 	}
@@ -40,30 +61,260 @@ func TestSimpleConfig(t *testing.T) {
 		t.Errorf("Invalid db `%s`, expected `%s`", config.Backup["test"].Db, "tmp.db")
 	}
 
-	if config.Backup["test"].Exclude != "*.png,*.tiff" {
-		t.Errorf("Invalid exclude `%s`, expected `%s`", config.Backup["test"].Exclude, "*.png,*.tiff")
+	if !compareInclusions(config.Backup["test"].Exclude, []string{"*.png", "*.tiff"}) {
+		t.Errorf("Invalid exclude `%+v`, expected `%+v`", config.Backup["test"].Exclude, []string{"*.png", "*.tiff"})
 	}
 
-	if config.Backup["test"].Include != "*.jpg" {
-		t.Errorf("Invalid exclude `%s`, expected `%s`", config.Backup["test"].Include, "*.jpg")
+	if !compareInclusions(config.Backup["test"].Include, []string{"*.jpg"}) {
+		t.Errorf("Invalid exclude `%+v`, expected `%+v`", config.Backup["test"].Include, []string{"*.jpg"})
 	}
 }
 
-func TestThreadsMissing(t *testing.T) {
+func TestNoConfigs(t *testing.T) {
+	configDef := `
+    [threads]
+    hash = 10
+    upload = 2
+
+    [aws]
+    access = 123abcAccess
+    secret = 123abcSecret
+`
+	if _, err := ReadConfig(configDef); err == nil || err.Error() != "No configurations given" {
+		t.Error("Expected error `No configurations given`")
+	}
+}
+
+func TestHashThreadsMissing(t *testing.T) {
 	configDef := `
     [aws]
-    access = abc123
-    secret = abc123
+    access = abc123Access
+    secret = abc123Secret
 
     [backup "test"]
     region = eu-west-1
     path = /tmp/
     db = tmp.db
-    exclude = *.png,*.tiff
-    include = *.jpg
 `
 
-	if _, err := ReadConfig(configDef); err == nil {
-		t.Error("ReadConfig should have complained about missing values for `Threads` section")
+	if _, err := ReadConfig(configDef); err == nil || err.Error() != "Need at least one hash thread" {
+		t.Error("ReadConfig should have complained about 0 value for hash threads")
 	}
+}
+
+func TestUploadThreadsMissing(t *testing.T) {
+	configDef := `
+    [threads]
+    hash = 10
+
+    [aws]
+    access = abc123Access
+    secret = abc123Secret
+
+    [backup "test"]
+    region = eu-west-1
+    path = /tmp/
+    db = tmp.db
+`
+
+	if _, err := ReadConfig(configDef); err == nil || err.Error() != "Need at least one upload thread" {
+		t.Error("ReadConfig should have complained about 0 value for upload threads: %s", err)
+	}
+}
+
+func TestAwsConfigInBackup(t *testing.T) {
+	configDef := `
+    [threads]
+    hash = 10
+    upload = 2
+
+    [aws]
+    access = abc123Access
+    secret = abc123Secret
+
+    [backup "test"]
+    region = eu-west-1
+    path = /tmp/
+    db = tmp.db
+    aws-access = 123abcAccess
+    aws-secret = 123abcSecret
+`
+
+	config, err := ReadConfig(configDef)
+	if err != nil {
+		t.Errorf("Unexpected error: %s", err)
+	}
+
+	if config.Backup["test"].Aws_Access != "123abcAccess" {
+		t.Errorf("Invalid AWS Access code `%s`, expected `%s`", config.Backup["test"].Aws_Access, "123abcAccess")
+	}
+
+	if config.Backup["test"].Aws_Secret != "123abcSecret" {
+		t.Errorf("Invalid AWS Secret `%s`, expected `%s`", config.Backup["test"].Aws_Secret, "123abcSecret")
+	}
+}
+
+func TestAwsConfigFromGlobal(t *testing.T) {
+	configDef := `
+    [threads]
+    hash = 10
+    upload = 2
+
+    [aws]
+    access = abc123Access
+    secret = abc123Secret
+
+    [backup "test"]
+    region = eu-west-1
+    path = /tmp/
+    db = tmp.db
+`
+	config, err := ReadConfig(configDef)
+	if err != nil {
+		t.Errorf("Unexpected error: %s", err)
+	}
+
+	if config.Backup["test"].Aws_Access != "abc123Access" {
+		t.Errorf("Invalid AWS Access code `%s`, expected `%s`", config.Backup["test"].Aws_Access, "abc123Access")
+	}
+
+	if config.Backup["test"].Aws_Secret != "abc123Secret" {
+		t.Errorf("Invalid AWS Secret `%s`, expected `%s`", config.Backup["test"].Aws_Secret, "abc123Secret")
+	}
+}
+
+func TestNoAwsConfig(t *testing.T) {
+	configDef := `
+    [threads]
+    hash = 10
+    upload = 2
+
+    [backup "test"]
+    region = eu-west-1
+    path = /tmp/
+    db = tmp.db
+`
+	if _, err := ReadConfig(configDef); err == nil || err.Error() != "No AWS credentials supplied for backup `test`" {
+		t.Error("Expected error about AWS credentials in `test` backup")
+	}
+}
+
+func TestAwsAccessOnly(t *testing.T) {
+	configDef := `
+    [threads]
+    hash = 10
+    upload = 2
+
+    [backup "test"]
+    region = eu-west-1
+    path = /tmp/
+    db = tmp.db
+    aws-access = abc123Access
+`
+	if _, err := ReadConfig(configDef); err == nil || err.Error() != "AWS Access code suplied, but no AWS Secret for config `test`" {
+		t.Error("Expected error about AWS credentials in `test` backup.")
+	}
+}
+
+func TestAwsSecretOnly(t *testing.T) {
+	configDef := `
+    [threads]
+    hash = 10
+    upload = 2
+
+    [backup "test"]
+    region = eu-west-1
+    path = /tmp/
+    db = tmp.db
+    aws-secret = abc123Secret
+`
+	if _, err := ReadConfig(configDef); err == nil || err.Error() != "AWS Secret suplied, but no AWS Access code for config `test`" {
+		t.Error("Expected error about AWS credentials in `test` backup.")
+	}
+}
+
+func TestInvalidRegion(t *testing.T) {
+	configDef := `
+    [threads]
+    hash = 10
+    upload = 2
+
+    [aws]
+    access = 123abcAccess
+    secret = 123abcSecret
+
+    [backup "test"]
+    region = xx-yyyy-999
+    path = /tmp/
+    db = tmp.db
+`
+	if _, err := ReadConfig(configDef); err == nil || err.Error() != "Unable to find region xx-yyyy-999" {
+		t.Error("Expected error about non-existing AWS region in `test` backup")
+	}
+}
+
+func TestNoRegion(t *testing.T) {
+	configDef := `
+    [threads]
+    hash = 10
+    upload = 2
+
+    [aws]
+    access = 123abcAccess
+    secret = 123abcSecret
+
+    [backup "test"]
+    path = /tmp/
+    db = tmp.db
+`
+	if _, err := ReadConfig(configDef); err == nil || err.Error() != "No region supplied for config `test`" {
+		t.Error("Expected error about non-existing AWS region in `test` backup")
+	}
+}
+
+func TestNoPath(t *testing.T) {
+	configDef := `
+    [threads]
+    hash = 10
+    upload = 2
+
+    [aws]
+    access = 123abcAccess
+    secret = 123abcSecret
+
+    [backup "test"]
+    region = eu-west-1
+    db = test.db
+`
+	if _, err := ReadConfig(configDef); err == nil || err.Error() != "No path supplied for config `test`" {
+		t.Error("Expected error missing path param for `test` backup: %s")
+	}
+}
+
+func TestNoDb(t *testing.T) {
+	configDef := `
+    [threads]
+    hash = 10
+    upload = 2
+
+    [aws]
+    access = 123abcAccess
+    secret = 123abcSecret
+
+    [backup "test"]
+    region = eu-west-1
+    path = /tmp/
+`
+	if _, err := ReadConfig(configDef); err == nil || err.Error() != "No db supplied for config `test`" {
+		t.Error("Expected error missing db param for `test` backup")
+	}
+}
+
+func compareInclusions(test, compare []string) bool {
+	for i, val := range test {
+		if compare[i] != val {
+			return false
+		}
+	}
+	return true
 }
