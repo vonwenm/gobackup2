564c1e99f2fc6d9693d7a646f1ca8f257632c84e
Remon van de Kamp <rpkamp@gmail.com>
2015-04-13 21:41:43 +0200

    Handle incoming errors in ListFiles, improved regex used by ListFiles

--- listfiles.go
+++ listfiles.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"os"
 	"path/filepath"
 	"regexp"
@@ -9,62 +10,79 @@ import (
 
 /**
  * ListFiles lists all files in a given path recursively.
- * It omits and empty files. When passed a slice of includes,
+ * It omits directories and empty files. When passed a slice of includes,
  * filenames must match one of the includes to be returned.
  * When passed a slice of excludes filenames must
- * not match any of the excludes, otherwise they won't be return
+ * not match any of the excludes, otherwise they won't be return.
  * If a filename matches both include and exclude, it will be excluded.
+ * This function closes the channel when it's done looping all files.
  * @param path string The path to scan
  * @param include []string Slice of include patterns
  * @param exclude []string Slice of exclude patterns
  * @param out <-chan *File
  */
 func ListFiles(path string, include, exclude []string, out chan<- *File) {
-	inRegex := regexify(include, ".*")
-	exRegex := regexify(exclude, "")
+	inRegex, exRegex := regexify(include), regexify(exclude)
 
 	go func() {
-		filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+		filepath.Walk(path, func(path string, info os.FileInfo, err error) (outErr error) {
+			if err != nil {
+				if info.IsDir() {
+					log.Printf("Error reading directory %s: %s. Skipping.", path, err)
+					return filepath.SkipDir
+				} else {
+					log.Printf("Error reading file %s: %s. Skipping file.", path, err)
+					return
+				}
+			}
+
 			if info.IsDir() || info.Size() == 0 {
-				return nil
+				return
 			}
 
 			if inRegex != nil && !inRegex.Match([]byte(path)) {
-				return nil
+				return
 			}
 
 			if exRegex != nil && exRegex.Match([]byte(path)) {
-				return nil
+				return
 			}
 
 			file := NewFile(path)
 			out <- file
-			return nil
+			return
 		})
 		close(out)
 	}()
 }
 
 /**
- * Regexify changes a list of human readable patterns to
+ * regexify changes a list of human readable patterns to
  * a regular expression. The regular expression is case insensitive
  * to make up for different capitalisation in filenames.
  * i.e. ["*.jpg", "*.png"] becomes ^.*\.jpg$|^.*\.png$
  * @param patterns []string Input patterns
- * @param ifEmpty string The default input pattern when the patterns slice is empty
  * @return
  */
-func regexify(patterns []string, ifEmpty string) *regexp.Regexp {
+func regexify(patterns []string) *regexp.Regexp {
 	var str string
 	if len(patterns) == 0 {
 		return nil
-	} else {
-		for i := range patterns {
-			patterns[i] = strings.Replace(patterns[i], ".", "\\.", -1)
-			patterns[i] = strings.Replace(patterns[i], "*", ".*", -1)
-			patterns[i] = "^" + patterns[i] + "$"
-		}
-		str = "(?i)" + strings.Join(patterns, "|")
 	}
+
+	// first copy patterns so we don't change
+	// them outside our function
+	p := make([]string, len(patterns))
+	copy(p, patterns)
+
+	// now loop over the patterns and turn them into a
+	// (sub) regular expressions
+	for i := range p {
+		p[i] = strings.Replace(p[i], ".", "\\.", -1)
+		p[i] = strings.Replace(p[i], "*", ".*", -1)
+	}
+
+	// now glue the regular expression parts together
+	str = "(?i)^" + strings.Join(p, "|") + "$"
 	return regexp.MustCompile(str)
 }
