40a51fca3586110fa702e5e49762d6057ab85611
Remon van de Kamp <rpkamp@gmail.com>
2015-05-20 20:38:58 +0200

    Finished up Uploader

--- uploader.go
+++ uploader.go
@@ -8,12 +8,20 @@ import (
 	"strings"
 )
 
+/**
+ * Uploader is responsible for uploading files to AWS Glacier
+ */
 type Uploader struct {
 	conn       *glacier.Connection
 	vault      string
 	indexVault string
 }
 
+/**
+ * NewUploader creates a new uploader instance
+ * and makes sure all needed vaults exist. If they don't exist
+ * they will be created
+ */
 func NewUploader(awsSecret, awsAccess string, awsRegion *aws.Region, vault string) (*Uploader, error) {
 	if strings.HasSuffix(vault, "_index") {
 		return nil, errors.New("Vault names can not end in `_index`")
@@ -54,4 +62,32 @@ func NewUploader(awsSecret, awsAccess string, awsRegion *aws.Region, vault strin
 		vault:      vault,
 		indexVault: indexVault,
 	}, nil
+}
+
+/**
+ * UploadFile tries to upload a file to AWS glacier.
+ * Will bail after 3 failed attempts.
+ */
+func (u *Uploader) UploadFile(path string) (string, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	defer func() {
+		if r := recover(); r != nil {
+			log.Printf("Error uploading file %s: %s\n", path, r)
+		}
+	}()
+
+	for retries := 1; retries <= 3; retries++ {
+		f.Seek(0, 0)
+		if amazonId, err := u.conn.UploadArchive(u.Vault, f, path); err != nil {
+			if retries == 3 {
+				return "", fmt.Errorf("Upload failed after 3 retries: %s", err)
+			}
+		} else {
+			return amazonId
+		}
+	}
 }
