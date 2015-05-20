7fa4d7f090088127b3409b0eb8a0f63e7f174103
Remon van de Kamp <rpkamp@gmail.com>
2015-05-20 21:10:29 +0200

    Started on main loop

--- main.go
+++ main.go
@@ -1,9 +1,35 @@
 package main
 
 import (
-	"fmt"
+	"flag"
+	"io/ioutil"
+	"log"
 )
 
 func main() {
-	fmt.Println("Go Backup V2")
+	configFile := flag.String("config", "gobackup.ini", "Path to config file")
+	flag.Parse()
+
+	configDef, err := ioutil.ReadFile(*configFile)
+	if err != nil {
+		log.Fatalf("Error reading config file: %s", err)
+	}
+
+	config, err := ReadConfig(string(configDef))
+	if err != nil {
+		log.Fatalf("Error parsing config: %s", err)
+	}
+
+	for _, backup := range config.Backup {
+		_, err := NewUploader(backup.AwsSecret, backup.AwsAccess, backup.Region.Region, backup.Vault)
+		if err != nil {
+			log.Printf("Error creating uploader: %s", err)
+			continue
+		}
+
+		_, err = NewArchive(backup.Db)
+		if err != nil {
+			log.Printf("Error creating archive: %s", err)
+		}
+	}
 }
