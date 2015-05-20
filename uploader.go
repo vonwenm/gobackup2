447f4b62a4a76af3cb2e53f750fabccb83178721
Remon van de Kamp <rpkamp@gmail.com>
2015-05-20 21:08:25 +0200

    Use named parameter in uploader for better panic recover

--- uploader.go
+++ uploader.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/rdwilliamson/aws"
 	"github.com/rdwilliamson/aws/glacier"
+	"os"
 	"strings"
 )
 
@@ -68,26 +69,28 @@ func NewUploader(awsSecret, awsAccess string, awsRegion *aws.Region, vault strin
  * UploadFile tries to upload a file to AWS glacier.
  * Will bail after 3 failed attempts.
  */
-func (u *Uploader) UploadFile(path string) (string, error) {
+func (u *Uploader) UploadFile(path string) (amazonId string, err error) {
 	f, err := os.Open(path)
 	if err != nil {
-		return nil, err
+		return
 	}
 	defer f.Close()
 	defer func() {
 		if r := recover(); r != nil {
-			log.Printf("Error uploading file %s: %s\n", path, r)
+			err = r.(error)
 		}
 	}()
 
 	for retries := 1; retries <= 3; retries++ {
 		f.Seek(0, 0)
-		if amazonId, err := u.conn.UploadArchive(u.Vault, f, path); err != nil {
+		if amazonId, err = u.conn.UploadArchive(u.vault, f, path); err != nil {
 			if retries == 3 {
-				return "", fmt.Errorf("Upload failed after 3 retries: %s", err)
+				err = fmt.Errorf("Upload failed after 3 retries: %s", err)
+				return
 			}
 		} else {
-			return amazonId
+			return
 		}
 	}
+	return
 }
